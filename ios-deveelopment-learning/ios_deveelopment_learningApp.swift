1fe6face1eb725a9d5bb5f69a427aeae10d4bef2
gle.bushkaa <gleb.mokryy@gmail.com>
2024-06-05 09:28:14 +0300

    Initial Commit

---
+++ ios-deveelopment-learning/ios_deveelopment_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_deveelopment_learningApp.swift
+//  ios-deveelopment-learning
+//
+//  Created by Hlib Mokryi on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_deveelopment_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
